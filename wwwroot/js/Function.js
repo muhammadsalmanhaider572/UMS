84e33bd547aa8bc89bcdad0c7504696e7a1fd11f
MuhammadSalmanHaider <msalmanhaider572@gmail.com>
2026-08-04 09:09:12 +0500

    Add project files.

---
+++ wwwroot/js/Function.js
@@ -0,0 +1,31 @@
+﻿const menuLink = document.querySelector(".menu-link");
+
+const subMenu = document.querySelector(".sub-menu");
+
+menuLink.addEventListener("click", function () {
+
+    if (subMenu.style.display === "block") {
+
+        subMenu.style.display = "none";
+
+    }
+    else {
+
+        subMenu.style.display = "block";
+
+    }
+
+});
+// cAI Assistant
+
+const aiMenu = document.querySelector(".ai-menu-link");
+
+const aiItem = document.querySelector(".ai-menu-item");
+
+
+aiMenu.addEventListener("click", function () {
+
+    aiItem.classList.toggle("active");
+
+});
+
